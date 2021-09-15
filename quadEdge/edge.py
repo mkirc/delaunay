656ca240b70efcb003721861f62c74787c1d7b1c
mkirc <m.p.kirchner@gmx.de>
2021-09-15 13:14:16 +0200

    adds main edge classes

---
+++ quadEdge/edge.py
@@ -0,0 +1,33 @@
+class QuadEdge:
+
+    """Container for Edges, which can be accessed by get/setitem"""
+
+    def __init__(self, org, dest):
+
+        self.edges = [
+                Edge(parent=self, data=org)
+                , Edge(parent=self, index=1)
+                , Edge(parent=self, index=2, data=dest)
+                , Edge(parent=self, index=3)
+                ]
+        self.org = None
+        self.dest = None
+
+    def __getitem__(self, idx):
+
+        return self.edges[idx]
+
+    def __setitem__(self, idx, val):
+
+        self.edges[idx] = val
+
+class Edge:
+
+    """Actual edge, the main object we will do work with"""
+
+    def __init__(self, parent, index=0, data=None):
+
+        self.next = self
+        self.data = data
+        self.index = index
+        self.parent = parent
