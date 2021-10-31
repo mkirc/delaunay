5964fcbe600e092600d81a6d70ebd766800a105f
mkirc <m.p.kirchner@gmx.de>
2021-10-31 16:50:47 +0100

    forgot to add offset

--- delaunay.py
+++ delaunay.py
@@ -6,6 +6,7 @@ from quadEdge.primitives import *
 def merge(m, ldo, ldi, rdi, rdo):
 
     # 'Compute the lower common tangent of L and R'
+
     while True:
         if ccw(org(rdi), org(ldi), dest(ldi)): # leftOf
             ldi = lnext(ldi)
@@ -17,7 +18,6 @@ def merge(m, ldo, ldi, rdi, rdo):
     # 'Create a first cross edge basel from rdi.Org to ldi.Org'
     basel = m.connect(sym(rdi), ldi)
 
-    
     if org(ldi) == org(ldo):
         ldo = sym(basel)
     if org(rdi) == org(rdo):
@@ -36,12 +36,14 @@ def merge(m, ldo, ldi, rdi, rdo):
         'Locate the first L point (lcand.Dest) to be encountered by the rising bubble
          and delete L edges out of basel.Dest that fail the circle test.'
         """
-        print(org(basel), dest(basel))
+        print('Basel:')
+        debug(m, edges=True, next=True)
         lcand = onext(sym(basel))
         if valid(lcand):
-            print('Valid lcand')
+            print(f'Valid lcand: {lcand}, {org(lcand)},{dest(lcand)}')
+            debug(m, edges=True, next=True)
             while inCircle(dest(basel), org(basel), dest(lcand), dest(onext(lcand))):
-                print('in loop')
+                print('in loop l')
                 t = onext(lcand)
                 m.deleteEdge(lcand)
                 lcand = t
@@ -51,9 +53,9 @@ def merge(m, ldo, ldi, rdi, rdo):
         """
         rcand = oprev(basel)
         if valid(rcand):
-            print('Valid rcand')
+            # print(f'Valid rcand: {rcand}, {org(rcand)},{dest(rcand)}')
             while inCircle(dest(basel), org(basel), dest(rcand), dest(oprev(rcand))):
-                print('in loop')
+                print('in loop r')
                 t = oprev(rcand)
                 m.deleteEdge(rcand)
                 rcand = t
@@ -66,29 +68,30 @@ def merge(m, ldo, ldi, rdi, rdo):
         rvalid = valid(rcand)
         
         if not lvalid and not rvalid:
-            print('Found upper common tangent!')
+            print('Found upper common tangent!\n')
             break
 
         """
         'The next cross edge is to be connected to either lcand.Dest or rcand.Dest.
         If both are valid, then choose the appropriate one using the InCircle test:'
         """
-        if (not lvalid or rvalid and inCircle(dest(lcand), org(lcand), org(rcand), dest(rcand))):
+        if (not lvalid or rvalid and 
+                inCircle(dest(lcand), org(lcand), org(rcand), dest(rcand))):
 
             # 'Add cross edge basel from rcand.Dest to basel.Dest'
-            print('connected rcand')
+            # print(f'connected rcand {org(rcand)}, {dest(rcand)}')
             basel = m.connect(rcand, sym(basel)) 
 
-            debug(m)
+            # debug(m)
 
             
         else:
 
             # 'Add cross edge basel from basel.Org to lcand.Dest'
-            print('connected lcand')
+            # print(f'connected lcand {org(lcand)}, {dest(lcand)}')
             basel = m.connect(sym(basel), sym(lcand))
 
-            debug(m)
+            # debug(m)
 
     return
 
@@ -99,22 +102,23 @@ def delaunay(m, start, end, leftEdge=None, rightEdge=None, rows=None):
     by guibas & stolfi.
     """
 
+
     verts = m.vertices
     if start < (end - 2): # four or more points
 
-        xMin = verts[start]
-        xMax = verts[end]
+        # xMin = verts[start]
+        # xMax = verts[end]
 
         # divide points in two halves
-        split = (end - start) // 2
-
+        split = (end - start) // 2 + start
+        print(f'split! {split}')
+        
         # recurse down the halves 
         ldo, ldi = delaunay(m, start, split)
         rdi, rdo = delaunay(m, (split + 1), end)
 
         # merge the obtained halves
         merge(m, ldo, ldi, rdi, rdo) 
-
 
         # while org(ldo) != xMin:
         #     ldo = m.rprev(ldo)
@@ -133,13 +137,12 @@ def delaunay(m, start, end, leftEdge=None, rightEdge=None, rows=None):
         """
 
         a = m.makeEdge(verts[start], verts[end])
-        leftEdge = a
-        rightEdge = sym(a)
 
         if start == end:
-            print('Lonely Point.')
+            print('Error: Lonely Point.')
+            exit()
         
-        return [leftEdge, rightEdge]
+        return [a, sym(a)]
 
     else: # Three points
         """
@@ -148,16 +151,14 @@ def delaunay(m, start, end, leftEdge=None, rightEdge=None, rows=None):
           Create edges a connecting s1 to s2 and b connecting s2 to s3'
         """
 
-        v1 = verts[start]
-        v2 = verts[start + 1]
-        v3 = verts[end]
-        
+        assert len(verts[start:end + 1]) == 3 # just in case
+
+        v1, v2, v3 = verts[start:end + 1]
         a = m.makeEdge(v1, v2)
         b = m.makeEdge(v2, v3)
         splice(sym(a), b)
 
         """'Now close the triangle'"""
-
         
         if ccw(v1, v3, v2):
             c = m.connect(b, a)
@@ -165,8 +166,9 @@ def delaunay(m, start, end, leftEdge=None, rightEdge=None, rows=None):
 
         elif ccw(v1, v2, v3): 
             c = m.connect(b, a)
-
             return [a, sym(b)]
-        else:# points are colinear
+
+        else: # points are colinear
             m.deleteEdge(c)
+            return [a, sym(b)]
 
