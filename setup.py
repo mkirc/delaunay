e0bc46867a27da8373ad647deda37390de6a0f89
mkirc <m.p.kirchner@gmx.de>
2022-01-06 19:32:37 +0100

    bump version for README update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="delaunay",
-    version="1.0.2",
+    version="1.0.3",
     author="mkirc",
     author_email="m.p.kirchner@gmx.de",
     description="A lightweight delaunay triangulation implementation in python",
