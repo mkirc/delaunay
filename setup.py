db56c4e8f60b2817cbd55f4029da64a108394414
mkirc <m.p.kirchner@gmx.de>
2022-01-06 19:27:48 +0100

    bump for packaging fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="delaunay",
-    version="1.0.1",
+    version="1.0.2",
     author="mkirc",
     author_email="m.p.kirchner@gmx.de",
     description="A lightweight delaunay triangulation implementation in python",
